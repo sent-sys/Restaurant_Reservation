aacc10c18d1c69dec4b96c15aa6e6821226d715b
Matthew Ho <mattewho37@gmail.com>
2021-08-24 15:22:35 -0400

    pushing to testing branch

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,16 @@
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const service = require("./reservations.service");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  res.json({ data: await service.list() });
 }
 
+async function create(req, res, next) {}
+
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
+  create: asyncErrorBoundary(create),
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -3,10 +3,12 @@
  *
  * @type {Router}
  */
-
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
+
+router.route("/new").post(controller.create);
 
 module.exports = router;
